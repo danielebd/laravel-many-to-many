763271fdd412c255a792bf26b7147378151c5ab3
danielebd <danielebalebalestro@gmail.com>
2023-05-24 01:52:12 +0200

    creare la migration per la tabella pivot project_technology

--- database/migrations/2023_05_23_232431_create_project_technologie_table.php
+++ database/migrations/2023_05_23_232431_create_project_technologie_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('project_technologie', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('technologie_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
